9505f202919c04da3cb13bf29d3db1b8df9f268f
MurrayMoffatt <31456197+MurrayMoffatt@users.noreply.github.com>
2024-12-05 13:43:06 +1300

    Do exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+  let result = "";
+  for (let i = inputString.length - 1; i > -1; i--) {
+    result += inputString[i];
+  }
+  return result;
 };
 
 // Do not edit below this line

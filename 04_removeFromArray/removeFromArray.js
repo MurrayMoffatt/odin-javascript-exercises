f51385d2881064e3e82a18139fce0a0dffd61384
MurrayMoffatt <31456197+MurrayMoffatt@users.noreply.github.com>
2024-12-16 14:10:48 +1300

    Do exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,30 @@
 const removeFromArray = function() {
+  if (arguments.length < 2) {
+    console.error("Invalid number of parameters supplied");
+    return;
+  }
+  if (! Array.isArray(arguments[0])) {
+    console.error("First parameter must be an array");
+    return;
+  }
+
+  const srcArray = arguments[0];
+  let elementToRemove;
+  let index;
+
+  for (let i = 1; i < arguments.length; i++) {
+    elementToRemove = arguments[i];
+    while (true) {
+      index = srcArray.indexOf(elementToRemove);
+      if (index == -1) {
+        break;
+      } else {
+        srcArray.splice(index, 1); // 2nd parameter means remove one item only
+      }
+    }
+  }
+
+  return srcArray;
 };
 
 // Do not edit below this line

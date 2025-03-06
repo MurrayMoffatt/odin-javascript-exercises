ce91beba6d3695df5a92bd59c882c45571091d07
MurrayMoffatt <31456197+MurrayMoffatt@users.noreply.github.com>
2025-03-06 16:21:42 +1300

    Do exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if (isNaN(n)) return "OOPS";
+  if (n < 0) return "OOPS";
+  if (n == 0) return 0;
+  let fib = 0;
+  let num1 = 1;
+  let num2 = 0;
+  for (let i = 2; i <= n; i++) {
+    fib = num1 + num2;
+    num2 = num1;
+    num1 = fib;
+  }
+  return num1;
 };
 
 // Do not edit below this line

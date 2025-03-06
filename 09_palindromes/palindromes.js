2d52d952cb7abcabfdc29c7532219376b824328c
MurrayMoffatt <31456197+MurrayMoffatt@users.noreply.github.com>
2025-03-06 15:01:26 +1300

    Do exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+  let lower = text.toLowerCase();
+  let clean = lower.replace(/[^a-z0-9]/g, '');
+  let reverse = clean.split("").reverse().join("");
+  return clean === reverse;
 };
 
 // Do not edit below this line
